1a541b04ea471ccb6dc7e7174e964f07a8b1cfc5
minthai222@gmail.com <minthai222@gmail.com>
2022-11-30 13:59:22 +0700

    Le Hoai Viet update

---
+++ saleappv1/saleapp/static/js/cart.js
@@ -0,0 +1,40 @@
+function addToCart(id, name, price) {
+    fetch('/api/cart', {
+        method: "post",
+        body: JSON.stringify({
+            "id": id,
+            "name": name,
+            "price": price
+        }),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).then(res => res.json()).then(data => {
+        console.info(data)
+        let d = document.getElementsByClassName("cart-counter")
+        for (let i = 0; i < d.length; i++)
+            d[i].innerText = data.total_quantity
+    }) // js promise
+}
+
+
+function updateCart(productId, obj) {
+    fetch(`/api/cart/${productId}`, {
+        method: "put",
+        body: JSON.stringify({
+            "quantity": obj.value
+        }),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).then(res => res.json()).then(data => {
+        console.info(data)
+        let d = document.getElementsByClassName("cart-counter")
+        for (let i = 0; i < d.length; i++)
+            d[i].innerText = data.total_quantity
+
+        let d1 = document.getElementsByClassName("cart-amount")
+        for (let i = 0; i < d1.length; i++)
+            d1[i].innerText = data.total_amount.toLocaleString("en-US")
+    }) // js promise
+}
